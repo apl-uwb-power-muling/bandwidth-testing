822ad2286150dd7235ebcddc42de54f30f5e22a3
Aaron Marburg <amarburg@apl.washington.edu>
2017-03-03 13:40:38 -0800

    Modified scripts a bit.

--- bandwidth_testing/iperf.sh
+++ bandwidth_testing/iperf.sh
@@ -4,5 +4,4 @@ REN=192.168.17.5
 STIMPY=192.168.17.6
 ODROID=192.168.17.7
 
-iperf3 --client $ODROID  -t 120 | ts
-iperf3 --client $ODROID -r  -t 120 | ts
+iperf3 --client $ODROID  -t 600 | ts
--- bandwidth_testing/monitor_stimpy.sh
+++ bandwidth_testing/monitor_stimpy.sh
@@ -2,4 +2,4 @@
 
 HOST=192.168.17.6
 
-ssh admin@$HOST  "/interface wireless registration-table print detail stats without-paging interval=1" | ts
+ssh admin@$HOST  "/interface wireless registration-table print detail stats without-paging interval=1" | ts 
