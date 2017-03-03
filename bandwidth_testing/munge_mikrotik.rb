9ca7bee03ac526c046ad96feeaaf32281330b94a
Aaron Marburg <amarburg@apl.washington.edu>
2017-03-03 15:44:54 -0800

    Updated the Mikrotik munger.

--- bandwidth_testing/munge_mikrotik.rb
+++ bandwidth_testing/munge_mikrotik.rb
@@ -5,30 +5,29 @@ infilename = ARGV.pop
 
 abort "usage:   munge_mikrotik.rb <filename>" if !infilename
 
-p infilename
-
-last_stamp = ""
+reset = true
 File.open( infilename, 'r' ) { |f|
 
   begin
-  while line = f.readline do
+    while line = f.readline do
 
-    stamp,rest = line.chomp.split(/\s{2,}/)
-
-    if stamp != last_stamp
-      last_stamp = stamp
+      stamp,rest = line.strip.split(/\s{2,}/)
 
       if rest
-        puts
-        print stamp, "  "
+        if reset
+          reset = false
+          print stamp, " "
+        end
+        print rest
+      else
+        print "\n" unless reset
+        reset = true
       end
-    end
-    print rest if rest and rest.length() > 0
 
+    end
+  rescue EOFError
+    puts
+    break
   end
-rescue EOFError
-  puts
-  break
-end
 
 }
